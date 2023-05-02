43aded9add291de4908d048f7df216e06a8f906e
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-05-02 14:42:45 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import {AppBar, Box, Button, Tab, Tabs, Toolbar, Typography} from "@mui/material"
 import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
@@ -12,54 +11,30 @@ const Header = () => {
     localStorage.removeItem("id");
     navigate("/");
   };
+  function MouseOver(event) {
+    event.target.style.color = 'yellow';
+
+  }
+  function MouseOut(event){
+    event.target.style.color="white";
+  }
   return (
     <div>
-      <AppBar position='sticky' sx={{background:"linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,9,121,0.9752275910364145) 35%, rgba(0,212,255,1) 100%)"}}>
-      <Toolbar>
-        <Typography variant='h4'>Blog App</Typography>
-       <Box display={'flex'} marginRight={"auto"} marginLeft={'auto'}>  
-          <Tabs textColor="inherit" value={value} onChange={(e,val)=>setValue(val)}>
-            <Tab LinkComponent={Link} to='/portal/allPosts' label='All Blogs'/>
-            <Tab LinkComponent={Link} to={`/portal/myPosts/${localStorage.getItem("id")}`} label='My Blogs'/>
-            <Tab LinkComponent={Link} to='/portal/addPost' label='Add Blog'/>
-
-          </Tabs>
-        </Box>
-        <Box display={"flex"} marginLeft={"auto"}>
-         <Button variant='contained' sx={{margin:1, borderRadius:10}} onClick={doLogout}>logout</Button> 
-        </Box>
-      </Toolbar>
-    </AppBar>
+     <div style={{background:"linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,9,121,0.9752275910364145) 35%, rgba(0,212,255,1) 100%)"}}>
+    <div className='container' >
+    <nav class="navbar navbar-expand" >
+  <h2 style={{color:'white' }}>Blog App</h2>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav" style={{marginLeft:'auto',marginRight:'auto',fontWeight:'bold'}}>
+      <Link style={{color:'white'}} to='/portal/allPosts' class="nav-link" onMouseOver={MouseOver} onMouseOut={MouseOut}>ALL BLOGS</Link>
+      <Link style={{color:'white'}} to={`/portal/myPosts/${localStorage.getItem("id")}`} class="nav-link" onMouseOver={MouseOver} onMouseOut={MouseOut}>MY BLOGS</Link>
+      <Link style={{color:'white'}} to='/portal/addPost' class="nav-link" onMouseOver={MouseOver} onMouseOut={MouseOut}>ADD BLOG</Link>
+    </div>
+    <button type="button" class="btn btn-success" style={{marginLeft:'auto'}} onClick={doLogout}>logout</button>
+  </div>
+</nav>
+    </div></div>
     </div>
   )
 }
 
-export default Header
-// import { useSelector } from 'react-redux';
-// const Header = () => {
-//   const [value,setValue]=useState();
-//   const isLoggedIn = useSelector(state=>state.isLoggedIn);
-//   return (
-//     <>
-//     <AppBar position='sticky' sx={{background:"linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,9,121,0.9752275910364145) 35%, rgba(0,212,255,1) 100%)"}}>
-//       <Toolbar>
-//         <Typography variant='h4'>Blog App</Typography>
-//       { isLoggedIn && <Box display={'flex'} marginRight={"auto"} marginLeft={'auto'}>  
-//           <Tabs textColor="inherit" value={value} onChange={(e,val)=>setValue(val)}>
-//             <Tab LinkComponent={Link} to='/blogs' label='All Blogs'/>
-//             <Tab LinkComponent={Link} to='/myBlogs' label='My Blogs'/>
-//           </Tabs>
-//         </Box> }
-//         <Box display={"flex"} marginLeft={"auto"}>
-//          { !isLoggedIn && <><Button LinkComponent={Link} to='/login' variant='contained' sx={{margin:1, borderRadius:10}}>Login</Button>
-//           <Button LinkComponent={Link} to='/signup' variant='contained' sx={{margin:1, borderRadius:10}}>Signup</Button></>}
-
-//          { isLoggedIn && <Button variant='contained' sx={{margin:1, borderRadius:10}}>logout</Button> }
-//         </Box>
-//       </Toolbar>
-//     </AppBar>
-//     </>
-//   )
-// }
-
-// export default Header
